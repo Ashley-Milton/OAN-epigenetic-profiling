1945a3796cf198f7f09487a8d1432a9f5dd84343
Ashley Milton <a.milton@unsw.edu.au>
2025-01-29 20:07:57 +1100

    Scripts 3

--- OAN_ChIP/8_OAN_ChIP_AMH_plots_generic.R
+++ OAN_ChIP/8_OAN_ChIP_AMH_plots_generic.R
@@ -1,4 +1,4 @@
-# Set the working directory
+#Set the working directory
 setwd("/path/to/indir")
 
 library(tidyverse)
@@ -7,49 +7,49 @@ library(scales)
 library(readr)
 library(grid)
 
-# Define histone variable
+#Define histone variable
 histones <- c("H3K4me3_german","H3K4me_german","H3K9me2","H3K9me3","H3K27ac_german","H3K27me3","H4K20me1")
 
-# Load the .gtf file and extract the positions for the AMHY gene
+#Load the .gtf file and extract the positions for the AMHY gene
 gtf <- read_delim("/path/to/genome/refgenome.gtf", 
                   delim = "\t", escape_double = FALSE, 
                   col_names = FALSE, trim_ws = TRUE, skip = 4)
 colnames(gtf) <- c("seqname", "source", "feature", "start", "end", "score", "strand", "frame", "attribute")
 
-# Filter for gene features and extract the AMHY gene
+#Filter for gene features and extract the AMHY gene
 amh_gene <- gtf %>% filter(feature == "gene" & grepl("AMH;", attribute))
 
-# Extract the start and end positions of the AMH gene
+#Extract the start and end positions of the AMH gene
 amh_start <- min(amh_gene$end)
 amh_end <- max(amh_gene$start)
 amh_chrom <- unique(amh_gene$seqname)
 
-# Define the region around the AMH TSS (10 kb upstream and downstream)
+#Define the region around the AMH TSS (10 kb upstream and downstream)
 region_start <- amh_start + 10000
 region_end <- amh_end - 1000
 
-# Load the lookup table
+#Load the lookup table
 lookup_table <- fread("/path/to/lookup_table.csv", col.names = c("old_name", "new_name"))
 
 for (histone in histones) {
   data_M <- fread(paste0("/path/to/macs2/output/M_", histone, "_treat_pileup.bdg"), header = FALSE)
   
-  # Rename columns to match the expected format
+  #Rename columns to match the expected format
   colnames(data_M) <- c("chromosome", "start", "end", "read_depth")
   
-  # Join with lookup table to rename chromosomes
+  #Join with lookup table to rename chromosomes
   data_M <- data_M %>% 
     left_join(lookup_table, by = c("chromosome" = "old_name")) %>% 
     mutate(chromosome = ifelse(is.na(new_name), chromosome, new_name)) %>% 
     select(-new_name)
   
-  # Filter for the chromosome of interest
+  #Filter for the chromosome of interest
   data_M <- data_M %>% filter(chromosome == amh_chrom)
   
-  # Convert start and end columns to numeric
+  #Convert start and end columns to numeric
   data_M <- data_M %>% mutate(across(c(start, end), as.numeric))
   
-  # TSS plots for AMHY
+  #TSS plots for AMHY
   data_M <- data_M %>% mutate(mod = histone, sex = "Male")
   
   data_M %>% 
